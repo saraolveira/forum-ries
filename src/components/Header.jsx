72006a170ad93f6ab0047f0a7409ceb178cd6b58
Sara Olveira <saralouro7@gmail.com>
2023-06-30 13:40:01 +0200

    menu links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,16 @@ const Header = () => {
 
     return <nav id="header">
     <img id="logo" src="/RIES-logo-negativo.png" />
-    <div id="links" className={clicked ? 'active' : ''}></div>
+    <div id="links" className={clicked ? 'active' : ''}>
+      <a href="">#RIES22</a>
+      <a href="">#RIES21</a>
+      <a href="">#RIES20</a>
+      <a href="">#RIES19</a>
+      <a href="">#RIES18</a>
+      <a href="">#RIES17</a>
+      <a href="">#RIES16</a>
+      <a href="https://forumries.com/exporpymes-2023/">EXPORPYMES</a>
+    </div>
     <FontAwesomeIcon icon={icon} size="2xl" id="menu-button" onClick={handleClick}/>
   </nav>
 }
