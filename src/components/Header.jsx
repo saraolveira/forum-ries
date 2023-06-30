63c2c05174a23a1e2dd8561a67d3218e0fe26bde
Sara Olveira <saralouro7@gmail.com>
2023-06-30 10:06:32 +0200

    quick fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
 
     const [clicked, setClicked] = useState(false)
+    const [icon, setIcon] = useState(faBars)
     const handleClick = () => {
       setClicked(!clicked)
+      if (!clicked) {
+        setIcon(faXmark)
+      } else {
+        setIcon(faBars)
+      }
+      
     }
 
     return <nav id="header">
     <img id="logo" src="/RIES-logo-negativo.png" />
     <div id="links" className={clicked ? 'active' : ''}></div>
-    <FontAwesomeIcon icon={faBars} size="2xl" id="menu-button" onClick={handleClick}/>
+    <FontAwesomeIcon icon={icon} size="2xl" id="menu-button" onClick={handleClick}/>
   </nav>
 }
 
