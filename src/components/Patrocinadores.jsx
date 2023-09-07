0ad74a1e97878f15774c8f9acdb43faa52023ac5
Sara Olveira <saralouro7@gmail.com>
2023-09-07 13:39:39 +0200

    traduccion sponsors

--- src/components/Patrocinadores.jsx
+++ src/components/Patrocinadores.jsx
@@ -1,10 +1,14 @@
+import { useTranslation } from "react-i18next"
 const Patrocinadores = () => {
+
+    const { t } = useTranslation()
+
     return (<div id="patrocinadores">
-        <h3>Dinamizador ágora</h3>
+        <h3>{t('patrocinios.ltype3')}</h3>
         <div className="block-patrocinadores">
             <a href="https://www.gradiant.org/" target="_blank"><img src="/patrocinadores/gradiant.png" alt="Gradiant" /></a>
         </div>
-        <h3>Mesa redonda</h3>
+        <h3>{t('patrocinios.ltype5')}</h3>
         <div className="block-patrocinadores">
             <a href="https://www.balidea.com/" target="_blank"><img src="/patrocinadores/balidea.png" alt="Balidea" /></a>
         </div>
