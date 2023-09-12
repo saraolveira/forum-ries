2f57bc531b84f1e4872d4856b44ef8061d89d09a
Sara Olveira <saralouro7@gmail.com>
2023-09-12 08:42:38 +0200

    patrocinadores

--- src/components/Patrocinadores.jsx
+++ src/components/Patrocinadores.jsx
@@ -4,8 +4,11 @@ const Patrocinadores = () => {
     const { t } = useTranslation()
 
     return (<div id="patrocinadores">
+        <h3>{t('patrocinios.ltype2')}</h3>
+            <a href="https://www.atendo.es/" target="_blank"><img src="/patrocinadores/atendo.png" alt="Atendo" /></a>
         <h3>{t('patrocinios.ltype3')}</h3>
         <div className="block-patrocinadores">
+            <a href="https://www.domusvi.es/" target="_blank"><img src="/patrocinadores/domusvi.png" alt="Gradiant" /></a>
             <a href="https://www.gradiant.org/" target="_blank"><img src="/patrocinadores/gradiant.png" alt="Gradiant" /></a>
             <a href="https://www.plexus.es/" target="_blank"><img src="/patrocinadores/plexus.png" alt="Plexus" /></a>
         </div>
