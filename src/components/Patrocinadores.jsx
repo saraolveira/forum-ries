4e55709bfa8884d692136d883c15be14dfa183a7
Sara Olveira <saralouro7@gmail.com>
2023-09-18 14:55:58 +0200

    patrocinador

--- src/components/Patrocinadores.jsx
+++ src/components/Patrocinadores.jsx
@@ -7,10 +7,11 @@ const Patrocinadores = () => {
         <h3>{t('patrocinios.ltype2')}</h3>
         <div className="block-patrocinadores">
             <a href="https://www.atendo.es/" target="_blank"><img src="/patrocinadores/atendo.png" alt="Atendo" /></a>
+            <a href="http://www.fundacionprofesornovoasantos.org/es/" target="_blank"><img src="/patrocinadores/fpns.png" alt="Fundación Profesor Novoa Santos" /></a>
         </div>
         <h3>{t('patrocinios.ltype3')}</h3>
         <div className="block-patrocinadores">
-            <a href="https://www.domusvi.es/" target="_blank"><img src="/patrocinadores/domusvi.png" alt="Gradiant" /></a>
+            <a href="https://www.domusvi.es/" target="_blank"><img src="/patrocinadores/domusvi.png" alt="DomusVi" /></a>
             <a href="https://www.gradiant.org/" target="_blank"><img src="/patrocinadores/gradiant.png" alt="Gradiant" /></a>
             <a href="https://www.plexus.es/" target="_blank"><img src="/patrocinadores/plexus.png" alt="Plexus" /></a>
         </div>
