890f49e23a14ae02df9a61b39b31303fd31d1244
Sara Olveira <saralouro7@gmail.com>
2023-09-28 09:20:53 +0200

    patrocinadores

--- src/components/Patrocinadores.jsx
+++ src/components/Patrocinadores.jsx
@@ -15,9 +15,17 @@ const Patrocinadores = () => {
             <a href="https://www.gradiant.org/" target="_blank"><img src="/patrocinadores/gradiant.png" alt="Gradiant" /></a>
             <a href="https://www.plexus.es/" target="_blank"><img src="/patrocinadores/plexus.png" alt="Plexus" /></a>
         </div>
+        <h3>{t('patrocinios.ltype4')}</h3>
+        <div className="block-patrocinadores">
+            <a href="https://insati.com/web/" target="_blank"><img src="/patrocinadores/insati.png" alt="Insati Innovation" /></a>
+        </div>
         <h3>{t('patrocinios.ltype5')}</h3>
         <div className="block-patrocinadores">
             <a href="https://www.balidea.com/" target="_blank"><img src="/patrocinadores/balidea.png" alt="Balidea" /></a>
+        </div>
+        <h3>{t('patrocinios.ltype6')}</h3>
+        <div className="block-patrocinadores">
+            <a href="https://pm4gov.com/" target="_blank"><img src="/patrocinadores/pm4gov.png" alt="PM4GOV" /></a>
         </div>
     </div>)
 }
